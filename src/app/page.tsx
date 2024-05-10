3ef3d27cbd8ec49e72bdd897c12a982b5c7c9a3c
truxish <justforgithubidk@gmail.com>
2024-05-10 22:40:50 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,7 @@ export default function Home() {
       </h1>
 
       <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-        Introducing Wormplain: The Ultimate Solution for Organic Growth on X
-        Are you tired of struggling to grow your X account? Look no further! With Wormplain, we have got you covered. Our innovative app is designed to help you effortlessly expand your X account organically, without any hassle.      
+        Are you tired of struggling to grow your X account? Our innovative app is designed to help you effortlessly expand your X account organically, without any hassle.      
       </p>
       
       <Link className= {buttonVariants({
@@ -35,9 +34,56 @@ export default function Home() {
 
     <div>
       <div className="relative isolate">
-        <div className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu"></div>
+        <div aria-hidden='true' className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+          <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }} 
+              className="relative-left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" />
+        </div>
+        <div>
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="mt-16 flow-root sm:mt-24">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <Image
+                src='/dashboard-preview.jpg'
+                alt='product preview'
+                width={1364}
+                height={866} 
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div aria-hidden='true' className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+          <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }} 
+              className="relative-left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]" />
+          </div>
       </div>
     </div>
+
+    <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+      <div className="mb-12 px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl sm:text-center">
+          <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+            Start tweeting in minutes
+          </h2>
+          <p className="mt-4 text-lg text-gray-600">
+            Tweeting has never been easier than with Wormplain.
+          </p>
+        </div>
+      </div>
+    </div>
+ 
+  
     </>
  
 
