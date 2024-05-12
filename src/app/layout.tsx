ed4f0c447a4555869bc949a9d2f321059038e286
truxish <justforgithubidk@gmail.com>
2024-05-12 22:13:30 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/util";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider> 
     <html lang="en" className="light">
       <body className={cn(
         'min=h=screen font-sans antialiseased grainy',
@@ -26,5 +28,6 @@ export default function RootLayout({
         {children}
         </body>
     </html>
+    </ClerkProvider>
   );
 }
